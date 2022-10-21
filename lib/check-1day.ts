130aaf93abf1c669447f00f3e2fa14f129e6cf96
hiroxto <hiroxto@gmail.com>
2022-10-21 20:47:08 +0900

    1日規制のチェッカーを実装

---
+++ lib/check-1day.ts
@@ -0,0 +1,47 @@
+import {LimitCheckResult} from "../types/app";
+import {CheckinItem} from "../types/foursquare";
+import dayjs from "dayjs";
+
+/**
+ * 1日(24時間)に50回でのチェックイン規制を確認するクラス
+ */
+export class Check1day {
+  /**
+   * チェックインの上限
+   */
+  readonly CHECKIN_LIMIT = 50;
+
+  constructor(private checkins: CheckinItem[], private now: Date) {
+  }
+
+  /**
+   * 1日(24時間)に50回のチェックインが行われているかをチェックする
+   */
+  check(): LimitCheckResult {
+    const day1ago = dayjs(this.now).add(-1, "day");
+    const matchCheckins: CheckinItem[] = [];
+
+    this.checkins.forEach(checkin => {
+      const isAfter = dayjs(checkin.createdAt).isAfter(day1ago);
+      if (isAfter) {
+        matchCheckins.push(checkin)
+      }
+    })
+
+    return {
+      checkins: matchCheckins,
+      checkinsCount: matchCheckins.length,
+      isLimited: this.isLimited(matchCheckins.length),
+    }
+  }
+
+  /**
+   * 与えたチェックイン数が規制上限に引っ掛かっているかを確認する
+   *
+   * @param checkinsCount
+   * @private
+   */
+  private isLimited(checkinsCount: number): boolean {
+    return checkinsCount >= this.CHECKIN_LIMIT;
+  }
+}
--- lib/limit-checker.ts
+++ lib/limit-checker.ts
@@ -2,6 +2,7 @@ import {CheckinItem} from "../types/foursquare";
 import {LimitCheckResult} from "../types/app";
 import {Check2min} from "./check-2min";
 import {Check15min} from "./check-15min";
+import {Check1day} from "./check-1day";
 
 export class LimitChecker {
   constructor(private checkins: CheckinItem[]) {
@@ -11,8 +12,11 @@ export class LimitChecker {
     const now = new Date();
     const check2min = new Check2min(this.checkins, now)
     const check15min = new Check15min(this.checkins, now)
+    const check1day = new Check1day(this.checkins, now)
+
     console.log(check2min.check())
     console.log(check15min.check())
+    console.log(check1day.check())
 
     return [
     ]
