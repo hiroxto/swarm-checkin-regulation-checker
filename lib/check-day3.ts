0110b81d4b19403b33d02b9b658061b7abe7b50a
hiroxto <hiroxto@gmail.com>
2022-10-21 20:53:47 +0900

    3日規制のチェッカーを実装

---
+++ lib/check-day3.ts
@@ -0,0 +1,47 @@
+import {LimitCheckResult} from "../types/app";
+import {CheckinItem} from "../types/foursquare";
+import dayjs from "dayjs";
+
+/**
+ * 3日(72時間)に90回でのチェックイン規制を確認するクラス
+ */
+export class Check3day {
+  /**
+   * チェックインの上限
+   */
+  readonly CHECKIN_LIMIT = 90;
+
+  constructor(private checkins: CheckinItem[], private now: Date) {
+  }
+
+  /**
+   * 3日(72時間)に90回のチェックインが行われているかをチェックする
+   */
+  check(): LimitCheckResult {
+    const day3ago = dayjs(this.now).add(-3, "days");
+    const matchCheckins: CheckinItem[] = [];
+
+    this.checkins.forEach(checkin => {
+      const isAfter = dayjs(checkin.createdAt * 1000).isAfter(day3ago);
+      if (isAfter) {
+        matchCheckins.push(checkin)
+      }
+    })
+
+    return {
+      checkins: matchCheckins,
+      checkinsCount: matchCheckins.length,
+      isLimited: this.isLimited(matchCheckins.length),
+    }
+  }
+
+  /**
+   * 与えたチェックイン数が規制上限に引っ掛かっているかを確認する
+   *
+   * @param checkinsCount
+   * @private
+   */
+  private isLimited(checkinsCount: number): boolean {
+    return checkinsCount >= this.CHECKIN_LIMIT;
+  }
+}
--- lib/limit-checker.ts
+++ lib/limit-checker.ts
@@ -3,6 +3,7 @@ import {LimitCheckResult} from "../types/app";
 import {Check2min} from "./check-2min";
 import {Check15min} from "./check-15min";
 import {Check1day} from "./check-1day";
+import {Check3day} from "./check-day3";
 
 export class LimitChecker {
   constructor(private checkins: CheckinItem[]) {
@@ -13,10 +14,12 @@ export class LimitChecker {
     const check2min = new Check2min(this.checkins, now)
     const check15min = new Check15min(this.checkins, now)
     const check1day = new Check1day(this.checkins, now)
+    const check3day = new Check3day(this.checkins, now)
 
     console.log(check2min.check())
     console.log(check15min.check())
     console.log(check1day.check())
+    console.log(check3day.check())
 
     return [
     ]
