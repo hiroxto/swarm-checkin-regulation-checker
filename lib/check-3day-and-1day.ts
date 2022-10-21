61124ebe5062842d1173e6eeca965b08074bd7e8
hiroxto <hiroxto@gmail.com>
2022-10-21 20:59:52 +0900

    3日規制と組み合わせる1日規制のチェッカーを作成

---
+++ lib/check-3day-and-1day.ts
@@ -0,0 +1,40 @@
+import {LimitCheckResult} from "../types/app";
+import {CheckinItem} from "../types/foursquare";
+import dayjs from "dayjs";
+
+/**
+ * 1日(24時間)に30回でのチェックイン規制を確認するクラス
+ */
+export class Check3dayAnd1day {
+  /**
+   * チェックインの上限
+   */
+  readonly CHECKIN_LIMIT = 30;
+
+  constructor(private checkins: CheckinItem[], private now: Date) {
+  }
+
+  /**
+   * 1日(24時間)に30回のチェックインが行われているかをチェックする
+   */
+  check(): LimitCheckResult {
+    const day1ago = dayjs(this.now).add(-1, "days");
+    const matchCheckins: CheckinItem[] = this.checkins.filter(checkin => dayjs(checkin.createdAt * 1000).isAfter(day1ago));
+
+    return {
+      checkins: matchCheckins,
+      checkinsCount: matchCheckins.length,
+      isLimited: this.isLimited(matchCheckins.length),
+    }
+  }
+
+  /**
+   * 与えたチェックイン数が規制上限に引っ掛かっているかを確認する
+   *
+   * @param checkinsCount
+   * @private
+   */
+  private isLimited(checkinsCount: number): boolean {
+    return checkinsCount >= this.CHECKIN_LIMIT;
+  }
+}
--- lib/limit-checker.ts
+++ lib/limit-checker.ts
@@ -4,6 +4,7 @@ import {Check2min} from "./check-2min";
 import {Check15min} from "./check-15min";
 import {Check1day} from "./check-1day";
 import {Check3day} from "./check-3day";
+import {Check3dayAnd1day} from "./check-3day-and-1day";
 
 export class LimitChecker {
   constructor(private checkins: CheckinItem[]) {
@@ -15,11 +16,13 @@ export class LimitChecker {
     const check15min = new Check15min(this.checkins, now)
     const check1day = new Check1day(this.checkins, now)
     const check3day = new Check3day(this.checkins, now)
+    const check3dayAnd1day = new Check3dayAnd1day(this.checkins, now)
 
     console.log(check2min.check())
     console.log(check15min.check())
     console.log(check1day.check())
     console.log(check3day.check())
+    console.log(check3dayAnd1day.check())
 
     return [
     ]
