a2f0740567623654bb815f93ec304d263b50a331
hiroxto <hiroxto@gmail.com>
2022-11-21 22:17:17 +0900

    規制状況を表示する部分を別コンポーネントに切り出し

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,11 @@ import {CheckinItem} from '../types/foursquare'
 import Footer from "../components/footer";
 import { LimitChecker } from "../lib/limit-checker";
 import {AllLimitCheckResult} from "../types/app";
-import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone"
 import { useRouter } from "next/router";
 import CheckinDetails from "../components/checkin-details";
+import LimitCheckResults from "../components/limit-check-results";
 dayjs.extend(timezone)
 dayjs.extend(require('dayjs/plugin/utc'))
 
@@ -66,9 +66,6 @@ const Home: NextPage = () => {
     console.log(checkins);
   }, [router, token, checkins]);
 
-  const notLimitedIcon = <CheckIcon className="h-6 w-6" aria-hidden="true" />
-  const limitedIcon = <ExclamationTriangleIcon className="h-6 w-6 text-red-300" aria-hidden="true" />
-
   return (
     <div className="bg-white py-12 px-10">
       <Head>
@@ -123,32 +120,7 @@ const Home: NextPage = () => {
         </div>
 
         { limitCheckResult !== null &&
-          <div className="mt-10 mb-5">
-            <h2 className="text-3xl font-semibold text-indigo-600">
-              規制状況
-            </h2>
-            <p className="mb-2">
-              { limitCheckResult.isLimited ? "規制されています" : "規制されていません" }
-            </p>
-
-            <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
-              {limitCheckResult.results.map((result, index) => (
-                <div key={index} className="relative">
-                  <dt>
-                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                      { result.isLimited ? limitedIcon : notLimitedIcon }
-                    </div>
-                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
-                      { result.title } : {result.isLimited ? "規制中" : "規制されていません"}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    対象チェックイン回数: { result.checkinsCount }
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+          <LimitCheckResults allLimitCheckResult={limitCheckResult}></LimitCheckResults>
         }
         { limitCheckResult !== null &&
           <CheckinDetails limitCheckResult={limitCheckResult}></CheckinDetails>
