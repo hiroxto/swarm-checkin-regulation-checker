e3fc2a9459cf10bf8f0cd7bc5ca4ff913cbeb89a
hiroxto <hiroxto@gmail.com>
2022-12-29 13:30:37 +0900

    規制されている際に規制状況のテキストを赤くするようにした

--- components/limit-check-results.tsx
+++ components/limit-check-results.tsx
@@ -7,13 +7,16 @@ interface Props {
 
 const LimitCheckResults  = (props: Props) => {
   const limitCheckResult = props.allLimitCheckResult;
+  const notLimitedTextColor = 'text-gray-900';
+  const limitedTextColor = 'text-red-500';
+  const textColor = limitCheckResult.isLimited ? limitedTextColor : notLimitedTextColor;
 
   return (
     <div className="mt-10 mb-5">
       <h2 className="text-3xl font-semibold text-indigo-600">
         規制状況
       </h2>
-      <p className="mb-2">
+      <p className={`mb-2 ${textColor}`}>
         { limitCheckResult.isLimited ? "規制されています" : "規制されていません" }
       </p>
 
