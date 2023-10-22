6c3276c6a606b7a14ee68f652642d98a5bedbd0c
Hiroto Kitazawa <hiroxto@gmail.com>
2023-10-23 01:51:35 +0900

    ESLintのルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,29 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 const eslintConfig = {
   extends: [
+    "eslint:recommended",
     "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "react/jsx-sort-props": "warn",
+    "no-console": "off",
+  },
 };
 
 module.exports = eslintConfig;
