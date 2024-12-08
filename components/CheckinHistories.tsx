5fec08469d3c4e0f66828e135de1c0cbba565bfc
Hiroto Kitazawa <hiroxto@gmail.com>
2024-12-08 23:55:20 +0900

    feat: 対象チェックインのフィルターを親コンポーネントで行いアコーディオンに件数を出す

--- components/CheckinHistories.tsx
+++ components/CheckinHistories.tsx
@@ -1,6 +1,7 @@
-import { endOfDay, startOfDay, subDays } from "date-fns";
+import { endOfDay, isWithinInterval, startOfDay, subDays } from "date-fns";
 import CheckinHistory from "~/components/CheckinHistory";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion";
+import { createdAt2Date } from "~/lib/functions";
 import type { CheckinItem } from "~/types/foursquare";
 
 interface Props {
@@ -9,16 +10,24 @@ interface Props {
 }
 
 const CheckinHistories = ({ checkins, now }: Props) => {
+  const filterCheckins = (checkins: CheckinItem[], periodFrom: Date, periodEnd: Date) =>
+    checkins.filter(checkin =>
+      isWithinInterval(createdAt2Date(checkin.createdAt), { start: periodFrom, end: periodEnd }),
+    );
+
   const d0start = startOfDay(now);
   const d0end = endOfDay(now);
+  const d0checkins = filterCheckins(checkins, d0start, d0end);
 
   const d1 = subDays(now, 1);
   const d1start = startOfDay(d1);
   const d1end = endOfDay(d1);
+  const d1checkins = filterCheckins(checkins, d1start, d1end);
 
   const d2 = subDays(now, 2);
   const d2start = startOfDay(d2);
   const d2end = endOfDay(d2);
+  const d2checkins = filterCheckins(checkins, d2start, d2end);
 
   const defaultValue = ["d0", "d1", "d2"];
   const targets = [
@@ -27,18 +36,21 @@ const CheckinHistories = ({ checkins, now }: Props) => {
       title: "本日のチェックイン一覧",
       periodFrom: d0start,
       periodTo: d0end,
+      checkins: d0checkins,
     },
     {
       key: "d1",
       title: "1日前のチェックイン一覧",
       periodFrom: d1start,
       periodTo: d1end,
+      checkins: d1checkins,
     },
     {
       key: "d2",
       title: "2日前のチェックイン一覧",
       periodFrom: d2start,
       periodTo: d2end,
+      checkins: d2checkins,
     },
   ];
 
@@ -49,12 +61,14 @@ const CheckinHistories = ({ checkins, now }: Props) => {
       <Accordion type="multiple" defaultValue={defaultValue}>
         {targets.map(target => (
           <AccordionItem value={target.key} key={target.key}>
-            <AccordionTrigger>{target.title}</AccordionTrigger>
+            <AccordionTrigger>
+              {target.title} ({target.checkins.length}件)
+            </AccordionTrigger>
             <AccordionContent>
               <CheckinHistory
                 title={target.title}
                 now={now}
-                checkins={checkins}
+                checkins={target.checkins}
                 periodFrom={target.periodFrom}
                 periodTo={target.periodTo}
               ></CheckinHistory>
--- components/CheckinHistory.tsx
+++ components/CheckinHistory.tsx
@@ -1,4 +1,3 @@
-import { isWithinInterval } from "date-fns";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
 import { createdAt2Date, date2String } from "~/lib/functions";
 import type { CheckinItem } from "~/types/foursquare";
@@ -12,18 +11,11 @@ interface Props {
 }
 
 const CheckinHistory = (props: Props) => {
-  const targetCheckins = props.checkins.filter(checkin =>
-    isWithinInterval(createdAt2Date(checkin.createdAt), {
-      start: props.periodFrom,
-      end: props.periodTo,
-    }),
-  );
-
   return (
     <div className="mb-5">
       <div className="sticky top-28 z-10 bg-white">
         <h3 className="text-2xl font-semibold text-indigo-400">{props.title}</h3>
-        <p>チェックイン数: {targetCheckins.length}</p>
+        <p>チェックイン数: {props.checkins.length}</p>
         <p className="mb-2">
           期間: {date2String(props.periodFrom)} から {date2String(props.periodTo)} まで
         </p>
@@ -37,7 +29,7 @@ const CheckinHistory = (props: Props) => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {targetCheckins.map((checkin, checkinIndex) => (
+            {props.checkins.map((checkin, checkinIndex) => (
               <TableRow key={checkin.id}>
                 <TableCell className="p-1 leading-1 border-r">{checkinIndex + 1}</TableCell>
                 <TableCell className="p-1 leading-1 border-r">
